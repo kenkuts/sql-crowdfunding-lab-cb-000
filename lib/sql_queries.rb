9fd25bdcb13f91f5d1aec8db2cc2b55e55717da4
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-08-13 22:17:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, pledges.amount FROM users
-JOIN projects ON users.id = pledges.user_id
+JOIN pledges ON users.id = pledges.user_id
 GROUP BY users.name ORDER BY pledges.amount ASC;"
 end
 
